2942016396968b02d0f55a21d14bdacc153cbe73
Cameron Adolph <cadolph@students.inghamisd.org>
2018-04-09 13:35:26 -0400

    reverted poorly merged file

--- src/com/company/Player1.java
+++ src/com/company/Player1.java
@@ -7,11 +7,36 @@ import java.util.Scanner;
 /**
  * Created by cadolph on 3/27/2018.
  */
-public class Player1  {
-// shuffle
-//  deal
-//    check or raise
-//
-
-
+public class Player1 extends Main {
+    int money = 50;
+    String card1;
+    String card2;
+    boolean x = true;
+    Scanner console = new Scanner(System.in);
+    public Player1(String Card1, String Card2){
+        card1 = Card1;
+        card2 = Card2;
+    }
+    public void action(String input){
+        if (input.equals("check")){
+            check();
+        } else if (input.equals("raise")) {
+            while (x) {
+                System.out.println("select amount to raise by.");
+                int amount = Integer.parseInt(console.nextLine());
+                if (amount < 5 || amount > money) {
+                    System.out.println("invalid amount");
+                }
+                else if(amount >= 5 && amount <= money){
+                    money = money-amount;
+                    System.out.println("player 1 has raised by "+amount+" Dollers");
+                    x = false;
+                }
+            }
+            x = true;
+        }
+    }
+    public void check(){
+        System.out.println("check");
+    }
 }
