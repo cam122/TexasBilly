f59b48647f20769afbc665e5b93d0b2a7a8f4205
Cameron Adolph <cadolph@students.inghamisd.org>
2018-04-09 14:06:32 -0400

    worked on CPU1

--- src/com/company/CPU1.java
+++ src/com/company/CPU1.java
@@ -3,7 +3,6 @@ package com.company;
 /**
  * Created by cadolph on 3/27/2018.
  */
-package com.company;
 
 import java.util.ArrayList;
         public class  CPU1 {
@@ -11,8 +10,22 @@ import java.util.ArrayList;
             public void ReceiveCard(card card) {
                 //add card to hand.
             }
-            
+            /*
+            returns a number: 0 means check
+            -1 means fold
+            positive number is amount raised
+            */
+           public int CheckRaiseFold() {
 
-        }
+           }
+            /*
+      returns a number: 0 means call
+      -1 means fold
+      positive number is amount raised by
+      */
+            public int CallRaiseFold(int amountNeededToCall) {
+
+
+            }
 }
 
