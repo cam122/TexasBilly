f1119d63384cd98b0b23cc5b72b9ed074a4399f8
Cameron Adolph <cadolph@students.inghamisd.org>
2018-04-17 11:42:43 -0400

    worked on cpu1

--- src/com/company/CPU1.java
+++ src/com/company/CPU1.java
@@ -4,17 +4,26 @@ package com.company;
  * Created by cadolph on 3/27/2018.
  */
 
+import javax.smartcardio.Card;
 import java.util.ArrayList;
         public class  CPU1 {
-            private Hand hand;
+            public  ArrayList<card> hand = new ArrayList <>();
             public void ReceiveCard(card card) {
                 //add card to hand.
-            }
-            /*
-            returns a number: 0 means check
-            -1 means fold
-            positive number is amount raised
-            */
+
+
+                }
+
+
+//            returns a number: 0 means check
+            int Number;
+
+
+                if check (Number = 0);
+                System.out.println("check");
+//            -1 means fold
+
+//            positive number is amount raised
            public int CheckRaiseFold() {
 
            }
