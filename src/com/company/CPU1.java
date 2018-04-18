1104229fce02320c79d3ec9f8e3382cd83e857b5
Cameron Adolph <cadolph@students.inghamisd.org>
2018-04-18 13:55:24 -0400

    cpu1

--- src/com/company/CPU1.java
+++ src/com/company/CPU1.java
@@ -6,24 +6,20 @@ package com.company;
 
 import javax.smartcardio.Card;
 import java.util.ArrayList;
-        public class  CPU1 {
+        public class  CPU1 extends basePlayer {
 
-            private int money;
+
             public  Hand playerCards;
 
-            public CPU1(int startmoney) {
-                money = startmoney;
+            public CPU1() {
+
 
                 playerCards = new Hand();
             }
 
-            /*
-      returns a number: 0 means call
-      -1 means fold
-      positive number is amount raised by
-      */
+
             public int CallRaiseFold(int amountNeededToCall) {
-                return 0;
+                call(amountNeededToCall);
             }
 
 
